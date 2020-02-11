72138bd221870ddb913d20794d738c3a023d923f
hwjung <hwjung@zinnotech.com>
2020-02-11 16:38:27 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace GetCountTypeMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var refAssem in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                var name  = Assembly.Load(new AssemblyName(refAssem.FullName));
+                int count = 0;
+                foreach(var type in name.DefinedTypes)
+                {
+                    count += type.GetMethods().Count();
+                }
+                Console.WriteLine($"types:{name.DefinedTypes.Count()} methods:{count} - assembly name:{refAssem.Name}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
